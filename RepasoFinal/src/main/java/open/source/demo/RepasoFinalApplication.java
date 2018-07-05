1f6cf136e6101e785fd11587cdafe8cc04a59396
RodrigoAGM <rod-gm@hotmail.com>
2018-07-05 00:35:00 -0500

    Created Project

---
+++ RepasoFinal/src/main/java/open/source/demo/RepasoFinalApplication.java
@@ -0,0 +1,12 @@
+package open.source.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepasoFinalApplication.class, args);
+    }
+}
